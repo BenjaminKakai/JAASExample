02673874f14a447f114a8cb6337441d9e295e0c9
Benjamin <Benjaminkakai001@gmail.com>
2024-06-17 12:03:13 +0300

    Updated this file UserPrincipal.java

--- src/com/baeldung/jaas/UserPrincipal.java
+++ src/com/baeldung/jaas/UserPrincipal.java
@@ -1,4 +1,32 @@
 package com.baeldung.jaas;
 
-public class UserPrincipal {
+import java.security.Principal;
+
+public class UserPrincipal implements Principal {
+
+    private String name;
+
+    public UserPrincipal(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserPrincipal that = (UserPrincipal) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
 }
