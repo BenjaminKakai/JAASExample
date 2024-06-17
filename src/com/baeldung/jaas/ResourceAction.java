48428987618a79cedcf684f8c34e9bab7e3b10e1
Benjamin <Benjaminkakai001@gmail.com>
2024-06-17 12:04:56 +0300

    Updated this file ResourceAction

--- src/com/baeldung/jaas/ResourceAction.java
+++ src/com/baeldung/jaas/ResourceAction.java
@@ -1,4 +1,16 @@
 package com.baeldung.jaas;
 
-public class ResourceAction {
+import java.security.PrivilegedAction;
+
+public class ResourceAction implements PrivilegedAction<Void> {
+
+    @Override
+    public Void run() {
+        SecurityManager sm = System.getSecurityManager();
+        if (sm != null) {
+            sm.checkPermission(new ResourcePermission("test_resource"));
+        }
+        System.out.println("I have access to test_resource !");
+        return null;
+    }
 }
