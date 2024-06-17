621a99d6921a4048f9f14b63a95841475352abeb
Benjamin <Benjaminkakai001@gmail.com>
2024-06-17 12:05:38 +0300

    Updated this file JaasAuthentication

--- src/com/baeldung/jaas/JaasAuthentication.java
+++ src/com/baeldung/jaas/JaasAuthentication.java
@@ -1,4 +1,21 @@
 package com.baeldung.jaas;
 
+import javax.security.auth.Subject;
+import javax.security.auth.login.LoginContext;
+import javax.security.auth.login.LoginException;
+import java.security.PrivilegedAction;
+
 public class JaasAuthentication {
+
+    public static void main(String[] args) {
+        try {
+            LoginContext loginContext = new LoginContext("jaasApplication", new ConsoleCallbackHandler());
+            loginContext.login();
+            Subject subject = loginContext.getSubject();
+            PrivilegedAction<Void> privilegedAction = new ResourceAction();
+            Subject.doAsPrivileged(subject, privilegedAction, null);
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
+    }
 }
